412fbb657df7856a76225c4aff159e3f49d0c2d6
jeronimo <jsequeiros@itba.edu.ar>
2022-06-21 19:02:48 -0300

    avances

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
-
 let Total = 0;
 
-function sumarprecio(precio){
+function sumarprecio(precio) {
   Total = Total + precio;
-   document.getElementById("Total").innerText = "TOTAL: $" + Total
-   document.getElementById("importeTotal").innerText = " $" + Total
-
+  document.getElementById("Total").innerText = "TOTAL: $" + Total;
+  document.getElementById("importeTotal").innerText = " $" + Total;
 }
 function Borrar() {
-  Total=0
+  Total = 0;
   document.getElementById("Total").innerHTML = "El total actual es: " + "$" + 0;
 }
 
-function mensajepago(){
-  if(document.forms["form"].username.value == "" || document.forms["form"].cardNumber.value == "" || document.forms["form"].month.value == "" || document.forms["form"].year.value == "" || document.forms["form"].cvv.value == ""){
-      alert("ERROR")
-  }else{
-      alert("Su pago fue realizado")
+function mensajepago() {
+  if (
+    document.forms["form"].username.value == "" ||
+    document.forms["form"].cardNumber.value == "" ||
+    document.forms["form"].month.value == "" ||
+    document.forms["form"].year.value == "" ||
+    document.forms["form"].cvv.value == ""
+  ) {
+    alert("ERROR");
+  } else {
+    alert("Su pago fue realizado");
   }
 }
